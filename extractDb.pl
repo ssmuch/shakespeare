1c06a1f5ce886ae2c681f27585a7ad28ed49513a
liyun <liyun@g7.com.cn>
2018-09-18 15:35:53 +0800

    update md file

--- extractDb.pl
+++ extractDb.pl
@@ -1,9 +1,14 @@
 use lib './lib';
+
 use DbUtil;
 use Operation;
 use Setup;
 
 my $db_file = shift;
+
+die "Please provide the path for the database to be accessed.\n" .
+    "Sample: perl $0 db\\test.db\n" if (not defined $db_file);
+
 Setup::export_env();
 my $dbh = DbUtil::connectDb($db_file);
 Operation::restore($dbh);
